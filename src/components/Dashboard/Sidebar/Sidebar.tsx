713afbec19f02db599b2b00c2f9df891af4115df
Daniel Ojeda <ojedadanielalejandro333@gmail.com>
2025-05-25 00:23:56 -0300

    refactor: eliminar importación innecesaria de React en Sidebar.tsx para optimizar el código

--- src/components/Dashboard/Sidebar/Sidebar.tsx
+++ src/components/Dashboard/Sidebar/Sidebar.tsx
@@ -1,5 +1,5 @@
 // src/components/Sidebar.jsx
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { NavLink, useNavigate, useLocation } from "react-router-dom";
 import {
     FaRegArrowAltCircleLeft,
