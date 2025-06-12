7c0f9a1fa09adcdee922786a0b2e7aac1ae908ba
Victor M Moreira <109500795+Pir4te64@users.noreply.github.com>
2025-06-12 19:02:55 -0300

    feat: mejorar el componente MobileMenu añadiendo nuevos íconos y enlaces de navegación, incluyendo un logo y ajustes en el diseño para una mejor experiencia de usuario en dispositivos móviles

--- src/components/navbar/MobileMenu.tsx
+++ src/components/navbar/MobileMenu.tsx
@@ -1,8 +1,10 @@
 // src/components/navbar/MobileMenu.tsx
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import { FaTimes } from 'react-icons/fa';
+import { FaTimes, FaHome, FaCalendarAlt, FaInfoCircle, FaSignInAlt, FaUserPlus, FaTachometerAlt, FaSignOutAlt, FaQuestionCircle } from 'react-icons/fa';
 import { useAuth } from '@/context/AuthContext';
+import logoPequeno from "@/assets/Logo.svg";
+import { Link } from 'react-router-dom';
 
 interface MobileMenuProps {
     isOpen: boolean;
@@ -13,38 +15,51 @@ const MobileMenu: React.FC<MobileMenuProps> = ({ isOpen, onClose }) => {
     const { isAuthenticated, logout } = useAuth();
 
     const linkClass = ({ isActive }: { isActive: boolean }) =>
-        `text-2xl ${isActive ? 'text-secondary' : 'text-white/80'} hover:text-secondary`;
+        `text-xl z-50 flex items-center gap-3 ${isActive ? 'text-secondary' : 'text-white/80'} hover:text-secondary`;
 
     return (
         <div
             className={`
         fixed inset-0
         bg-primary 
-        flex flex-col pt-16 items-center space-y-8 text-white z-50
+        flex flex-col pt-16 items-start px-4 space-y-6 text-white z-50
         transition-opacity duration-300 ease-in-out
         ${isOpen ? 'opacity-100' : 'opacity-0 pointer-events-none'}
       `}
         >
-            {/* Glow top-left */}
-            <div className="pointer-events-none absolute -left-10 top-0 h-40 w-40 rounded-full bg-secondary blur-3xl filter" />
-
-            {/* Glow bottom-right */}
-            <div className="pointer-events-none absolute -right-10 bottom-0 h-40 w-40 rounded-full bg-secondary blur-3xl filter" />
+            {/* Logo pequeño */}
+            <div className="absolute left-3 top-5">
+                <Link to="/" className="relative h-8 w-8">
+                    <span className="pointer-events-none absolute right-0 top-0 h-32 w-32 -translate-y-1/4 translate-x-1/4 transform rounded-full bg-secondary opacity-80 blur-3xl filter" />
+                    <img
+                        src={logoPequeno}
+                        alt="Logo Pequeño"
+                        className="relative z-10 w-full cursor-pointer object-cover"
+                    />
+                </Link>
+            </div>
 
             {/* Close button */}
-            <button onClick={onClose} className="absolute right-4 top-4">
+            <button onClick={onClose} className="absolute right-4 top-1">
                 <FaTimes className="h-6 w-6" />
             </button>
 
             {/* Nav links comunes */}
             <NavLink to="/" onClick={onClose} className={linkClass}>
+                <FaHome className="text-md" />
                 Inicio
             </NavLink>
             <NavLink to="/eventos" onClick={onClose} className={linkClass}>
+                <FaCalendarAlt className="text-md" />
                 Eventos
             </NavLink>
             <NavLink to="/sobre-voi" onClick={onClose} className={linkClass}>
+                <FaInfoCircle className="text-md" />
                 Sobre Voi
+            </NavLink>
+            <NavLink to="/faq" onClick={onClose} className={linkClass}>
+                <FaQuestionCircle className="text-md" />
+                FAQs
             </NavLink>
 
             {/* Acciones según auth */}
@@ -53,11 +68,9 @@ const MobileMenu: React.FC<MobileMenuProps> = ({ isOpen, onClose }) => {
                     <NavLink
                         to="/dashboard"
                         onClick={onClose}
-                        className={({ isActive }) =>
-                            `text-lg px-3 py-1.5 rounded-lg ${isActive ? 'bg-white text-primary' : 'bg-secondary text-white hover:opacity-90'
-                            }`
-                        }
+                        className={linkClass}
                     >
+                        <FaTachometerAlt className="text-md" />
                         Dashboard
                     </NavLink>
                     <button
@@ -65,21 +78,24 @@ const MobileMenu: React.FC<MobileMenuProps> = ({ isOpen, onClose }) => {
                             logout();
                             onClose();
                         }}
-                        className="mt-4 rounded-lg bg-red-600 px-4 py-2 text-lg font-semibold transition hover:bg-red-700"
+                        className="z-50 flex items-center gap-3 text-2xl text-white/80 hover:text-red-500"
                     >
-                        Cerrar sesión
+                        <FaSignOutAlt className="text-md" />
+                        Cerrar Sesión
                     </button>
                 </>
             ) : (
                 <>
                     <NavLink to="/login" onClick={onClose} className={linkClass}>
+                        <FaSignInAlt className="text-md" />
                         Iniciar Sesión
                     </NavLink>
                     <NavLink
                         to="/register"
                         onClick={onClose}
-                        className="mt-4 rounded-lg bg-secondary px-4 py-2 text-lg font-semibold hover:opacity-90"
+                        className={linkClass}
                     >
+                        <FaUserPlus className="text-md" />
                         Registrarse
                     </NavLink>
                 </>
